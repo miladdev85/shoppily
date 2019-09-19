c3a7d9d563933f7901f2818971e3c591aebee4f3
Milad Hashemi <milad.hashemi.dev@gmail.com>
2019-09-19 23:02:08 +0200

    CSS polishing

--- src/SectionItem.js
+++ src/SectionItem.js
@@ -1,11 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+const Overlay = styled.h2`
+  padding: 6px 8px 6px 8px;
+  text-align: center;
+  background-color: white;
+  color: grey;
+  cursor: pointer;
+  &:hover {
+    background-color: rgb(170, 170, 170);
+    color: white;
+  }
+`;
 
 const SectionItem = props => {
   return (
-    <div style={{ paddingBottom: 20 }} className="col-6 col-md-3 product__container">
-      <img src={props.imageUrl} alt={props.name} className="product__image" />
-      <p>{props.name}</p>
-      <p>{props.price} kr</p>
+    <div style={{ marginBottom: 20 }} className="col-6 col-md-3">
+      <div className="image__container">
+        <div style={{ backgroundImage: `url(${props.imageUrl})` }} className="product__image" />
+        <div className="product__overlay">
+          <Overlay>Add to cart</Overlay>
+          <Overlay>More info</Overlay>
+        </div>
+      </div>
+      <div style={{ cursor: "pointer" }} className="product__info">
+        <p className="product__name">{props.name}</p>
+        <p>{props.price} kr</p>
+      </div>
     </div>
   );
 };
