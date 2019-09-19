a8fa3d17eb767ca6893ea2e1897ee0ef79f9ad46
Milad Hashemi <milad.hashemi.dev@gmail.com>
2019-09-19 12:45:36 +0200

    Adding network location to env

--- src/Network.js
+++ src/Network.js
@@ -1,12 +1,12 @@
-export const baseUrl = "https://shrouded-meadow-49385.herokuapp.com";
+export const baseUrl = process.env.REACT_APP_BASE_URL;
 export const sections = `${baseUrl}/sectionlinks`;
 export const products = `${baseUrl}/products`;
 export const mainSections = `${baseUrl}/mainsections`;
 
 export const sectionId = {
-  accessoarer: "5d82a76e9c13b30017fd0845",
-  jackor: "5d82a6b99c13b30017fd0836",
-  skor: "5d82a7329c13b30017fd083f",
-  herr: "5d82a6d49c13b30017fd0839",
-  dam: "5d8344283850e50017d4efe5"
+  accessoarer: process.env.ACCESSOARER_ID,
+  jackor: process.env.JACKOR_ID,
+  skor: process.env.SKOR_ID,
+  herr: process.env.HERR_ID,
+  dam: process.env.DAM_ID
 };
