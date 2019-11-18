25c94b80e1aebed2d1bdac33162d4ff0cff184bf
Milad Hashemi <milad.hashemi.dev@gmail.com>
2019-11-18 22:06:10 +0100

    Reviewed FAQ and answers

--- src/Utils/data.js
+++ src/Utils/data.js
@@ -7,37 +7,32 @@ export default [
   {
     question: "Berätta om detta projekt",
     answer:
-      "Det här är mitt andra projekt. Efter Movify projektet ville jag lära mig mer om CSS och SCSS. CSS har varit något jag haft lite svårt för tidigare men i och med detta projekt känner jag mig mycket tryggare med den delen. Sidan är mycket enklare än Movify men CSS delen är mer komplicerat och har åstadkommits utan CSS Framework förutom enstaka tillfällen där jag använt mig av Bootstrap Grid för mobila vyer. Anledningen till att CSS varit svårt att greppa för mig är helt enkelt för att jag inte lagt ner tid på det förrän nu. Tidigare har JavaScript och React varit i huvudfokus."
+      "Efter Movify projektet ville jag lära fördjupa mig mer inom CSS och SCSS. CSS har varit något som hamnat lite i skymundan under min utvecklingsresa men i och med detta projekt känner jag mig mycket tryggare med det. Sidan är något enklare än Movify men CSS delen är mer komplicerat och har åstadkommits utan CSS Framework förutom enstaka tillfällen där jag använt mig av Bootstrap Grid för mobila vyer."
   },
   {
     question: "Vad har varit den stora utmaningen?",
     answer:
-      "Eftersom detta är mitt första projekt utan något CSS Framework har CSS delen varit den stora utmaningen. Positioneringen samt att få sidan att bli responsiv och fungera bra på mobilen. Sedan har kombinationen av ett Headless CMS + MongoDB + Heroku + Netlify haft sina egna utmaningar, men det har varit intressant och roligt när jag väl lyckats."
+      "Att tänka ut en design med hög användarupplevelse som fungerar bra på mobilen och på större skärmar. Sedan har kombinationen av ett Headless CMS + MongoDB + Heroku + Netlify haft sina egna utmaningar, men det har varit intressant och lärorikt."
   },
   {
     question: "Hur känner du inför React då?",
     answer:
-      "Jag känner mig trygg med grunderna i React och tycker att min grund nu börjar bli väldigt stadig och att jag är redo att ta nästa kliv i min utveckling."
+      "Jag känner mig trygg med React och tycker att min grund är väldigt stadig och att jag är redo att ta nästa kliv i min utveckling."
   },
   {
     question: "Vad är nästa steg?",
     answer:
-      'Nu ska jag bygga "self contained" komponenter som kan användas i andra projekt. Så som formulär och knappar. Kanske kan jag till och med göra denna FAQ sektion till en "self contained" komponent så att jag kan återanvända den i andra projekt.'
+      'Nästa steg är att bygga en bloggportal där jag kommer att använda mig av React Hooks, Context, Router samt Firestore. Vidare vill jag utöka mitt egna bibliotek med "self contained components" som kan återanvändas i kommande projekt.'
   },
   {
     question: "Något speciellt du vill skryta om i detta projekt?",
     answer:
-      'Ta en titt på "Logga in" sidan. Jag är ganska nöjd över mina formulär. Tycker de både ser bra ut och har bra funktionalitet samt går att återanvända.'
+      'Jag är nöjd med användandet av Context samt formulärerna på "Logga in" sidan. Jag tycker att formulärerna ser bra ut och har bra funktionalitet samt att de är enkla att återanvända i andra projekt.'
   },
   {
     question: "Berätta om plattformen för denna sida",
     answer:
-      "CMS är av typen Headless CMS och heter Strapi. Jag tyckte det lät intressant att ha ett eget CMS verktyg för att hantera produkterna, så jag satte mig in i dokumentationen för Strapi. Detta visar att jag kan läsa dokumentation och ge mig ut på okänd mark ;) Databasen består av MongoDB och back-enden är hostad på Heroku. På grund av att jag använder deras gratis version, kan den initiala laddningen av sidan ta en stund. Front-enden är deployad via Netlify."
-  },
-  {
-    question: "Vad vill du jobba med?",
-    answer:
-      'Jag vill jobba med front-end. Jag tycker det är roligt och givande att jobba med UI dock känner jag att jag behöver professionell stöd. Jag vill bemästra "self contained" konceptet. Jag vill veta där jag gjort eller tänkt fel och bygga vidare på min grund. Då jag är väldigt snabblärd är jag helt övertygad om att jag kommer att kunna ta nästa kliv i min utveckling inom en väldigt kort tidsram.'
+      "CMS är av typen Headless och heter Strapi. Jag tyckte det lät intressant att ha ett eget CMS verktyg för att hantera produkterna och satte mig därför in i dokumentationen för Strapi. Detta visar att jag kan läsa dokumentation och ge mig ut på okänd mark ;) Databasen består av MongoDB och back-enden är hostad på Heroku. På grund av att jag använder deras gratis version, kan den initiala laddningen av sidan ta en stund. Front-enden är deployad via Netlify."
   },
   {
     question: "Hur har du hanterat utmaningar som uppstått?",
@@ -45,13 +40,8 @@ export default [
       "Genom Google, YouTube och att läsa dokumentation. Jag är en visuell person och lär mig väldigt snabbt när jag t.ex. tittar på ett kort YouTube klipp om ämnet ifråga."
   },
   {
-    question: '"Mer info" och "Lägg till" knapparna gör ingenting?',
+    question: '"Mer info" knappen gör ingenting?',
     answer:
-      "Jag kände inte att dessa knappar skulle medföra något ytterligare i detta projekt och lämnade de för tillfället för att ta mig an nästa projekt - bygga self contained komponenter. I framtiden kanske dessa knappar leder till en modal eller ny sida för mer information om produkten samt en kundkorg. Men som sagt, det är endast nya sidor och skulle nog inte visa något mer i CV syfte eller imponera på någon."
-  },
-  {
-    question: "Övrigt som du själv vill tillägga?",
-    answer:
-      "Ja, faktiskt. Jag anser att jag haft några milstolpar under dessa 6 månader som jag studerat JavaScript och front-end utveckling. Det första var självklart att jag lärde mig JavaScript. Sedan kom React och JSX in i bilden - oj, vad jag älskade JSX när jag såg det. Men det allra viktigaste har kanske varit Bootstrap! Jag kunde äntligen positionera allt hur jag än ville. Jag var faktiskt nära på att lägga av innan jag lärde mig Bootstrap... Vilken tur att jag hittade Bootstrap ;) Men jag ville inte vara begränsad av Bootstrap och valde därför att lära mig mer om CSS och SCSS. Nu känner jag mig betydligt mer säker på mina CSS kunskaper och förstår att Bootstrap endast är ett verktyg som underlättar vissa delar och moment. Sedan tänker jag att på de flesta företag använder man någon typ av CSS framework för att ha samma standard. Användandet av Bootstrap CSS har medfört att jag nu kan skapa snygga sidor relativt fort och kan istället fokusera mer på att fördjupa mig ännu inom React."
+      "Jag kände inte att det skulle medföra något ytterligare i detta projekt och lämnade den för tillfället för att ta mig an nästa projekt. I framtiden kanske knappen leder till en modal eller ny sida för mer information om produkten."
   }
 ];
