aa15d0564a8624bf1936149d6fb47e4715889ca4
Milad Hashemi <milad.hashemi.dev@gmail.com>
2019-11-19 11:00:51 +0100

    Added note about no functionality on sign in page

--- src/SignInPage.js
+++ src/SignInPage.js
@@ -4,14 +4,19 @@ import NewUser from "./NewUser";
 
 const SignInPage = () => {
   return (
-    <div className="row" style={{ marginTop: "60px" }}>
-      <div className="col col-md-4 offset-lg-1" style={{ marginBottom: "40px" }}>
-        <SignIn />
+    <>
+      <h3 style={{ marginTop: "20px", textAlign: "center" }}>
+        Only for demonstration purpose. No sign in functionality.
+      </h3>
+      <div className="row" style={{ marginTop: "60px" }}>
+        <div className="col col-md-4 offset-lg-1" style={{ marginBottom: "40px" }}>
+          <SignIn />
+        </div>
+        <div className="col col-md-4 offset-lg-2">
+          <NewUser />
+        </div>
       </div>
-      <div className="col col-md-4 offset-lg-2">
-        <NewUser />
-      </div>
-    </div>
+    </>
   );
 };
 
