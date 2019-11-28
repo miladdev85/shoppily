a47504cac937e297a1595e730be6de687fd33881
Milad Hashemi <milad.hashemi.dev@gmail.com>
2019-11-28 13:53:14 +0100

    Fine tuning of box shadow

--- src/Contexts/CartContext.js
+++ src/Contexts/CartContext.js
@@ -3,44 +3,7 @@ import React, { createContext, useState } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [products, setProducts] = useState([
-    // {
-    //   amount: 2,
-    //   createdAt: "2019-09-19T09:46:03.525Z",
-    //   id: "5d834e5b3850e50017d4efea",
-    //   imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
-    //   mainsection: "5d82a76e9c13b30017fd0845",
-    //   name: "Blue Beanie",
-    //   price: 29,
-    //   updatedAt: "2019-09-19T20:55:21.733Z",
-    //   __v: 0,
-    //   _id: "5d834e5b3850e50017d4efea"
-    // },
-    // {
-    //   amount: 1,
-    //   createdAt: "2019-09-18T21:51:36.735Z",
-    //   id: "5d82a6e89c13b30017fd083c",
-    //   imageUrl: "https://i.ibb.co/dJbG1cT/yeezy.png",
-    //   mainsection: "5d82a7329c13b30017fd083f",
-    //   name: "Adidas Yeezy",
-    //   price: 3299,
-    //   updatedAt: "2019-09-19T20:59:29.928Z",
-    //   __v: 0,
-    //   _id: "5d82a6e89c13b30017fd083c"
-    // },
-    // {
-    //   amount: 5,
-    //   createdAt: "2019-09-19T09:55:42.896Z",
-    //   id: "5d83509e3850e50017d4efef",
-    //   imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-    //   mainsection: "5d82a76e9c13b30017fd0845",
-    //   name: "Red Beanie",
-    //   price: 89,
-    //   updatedAt: "2019-09-19T20:55:52.482Z",
-    //   __v: 0,
-    //   _id: "5d83509e3850e50017d4efef"
-    // }
-  ]);
+  const [products, setProducts] = useState([]);
 
   const sortArray = (array, value) => {
     return array.sort((a, b) => {
