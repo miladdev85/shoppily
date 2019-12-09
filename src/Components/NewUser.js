c2125a9fe49426ccaa6b6901319930fadd461515
Milad Hashemi <milad.hashemi.dev@gmail.com>
2019-12-09 18:40:17 +0100

    Fix bug that could lead to false password confirmation

--- src/Components/NewUser.js
+++ src/Components/NewUser.js
@@ -15,16 +15,21 @@ export class SignIn extends Component {
       fullName: "",
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      passwordMatch: ""
     },
     formValid: false
   };
 
   // Set state with input values and if there are any errors. We use these errors in the validateForm function which we run after setting state here.
+  // We use passwordMatch to check if the password and confirmation is the same.
+  // This is useful if the user first fills in password and confirmation and then changes password. Without this we had a bug that did not update confirmed and formvalidation.
+
   onChange = e => {
     e.preventDefault();
     const { name, value } = e.target;
     let errorMessage = "";
+    let passwordMatch = "";
 
     switch (name) {
       case "fullName":
@@ -35,9 +40,12 @@ export class SignIn extends Component {
         break;
       case "password":
         errorMessage = value.length < 8 ? "Lösenordet måste innehålla minst 8 tecken" : "";
+        passwordMatch =
+          value === this.state.inputs.confirmPassword ? "" : "Lösenordet matchar inte";
         break;
       case "confirmPassword":
         errorMessage = value !== this.state.inputs.password ? "Lösenordet matchar inte" : "";
+        passwordMatch = value === this.state.inputs.password ? "" : "Lösenordet matchar inte";
         break;
       default:
         break;
@@ -46,23 +54,27 @@ export class SignIn extends Component {
     this.setState(
       {
         inputs: { ...this.state.inputs, [name]: value },
-        errors: { ...this.state.errors, [name]: errorMessage }
+        errors: { ...this.state.errors, passwordMatch, [name]: errorMessage }
       },
       this.validateForm
     );
   };
 
   // Form validation is done here. Set state weither form is valid or not. We validate all fields before setting formValid - in case one field is left empty etc.
+  // We don't validate form if passwordMatch is not empty.
   validateForm = () => {
+    const { passwordMatch } = this.state.errors;
     let inputsValid = true;
     let noErrors = true;
+
     // Check if all inputs have a length higher than 1. If not, set inputsValid to false.
     Object.values(this.state.inputs).forEach(val => val.length < 1 && (inputsValid = false));
+
     // Check if all erorrs are empty. If not, set noErrors to false.
     Object.values(this.state.errors).forEach(val => val.length > 0 && (noErrors = false));
 
-    // If inputsValid true AND noErrors is true, we validate the form.
-    if (inputsValid && noErrors) {
+    // If inputsValid true AND noErrors AND passwordMatch is empty, set formValid to true and validate the form.
+    if (inputsValid && noErrors && !passwordMatch) {
       this.setState({ formValid: true });
     } else {
       this.setState({ formValid: false });
@@ -74,7 +86,11 @@ export class SignIn extends Component {
   };
 
   render() {
-    const { email, password, fullName, confirmPassword, errors, formValid } = this.state;
+    const {
+      inputs: { email, password, fullName, confirmPassword },
+      errors,
+      formValid
+    } = this.state;
 
     return (
       <div>
@@ -123,7 +139,7 @@ export class SignIn extends Component {
             name="confirmPassword"
             placeholder="Bekräfta lösenordet"
           >
-            {!formValid && <p>{errors.confirmPassword}</p>}
+            {!formValid && confirmPassword && errors.passwordMatch && <p>{errors.passwordMatch}</p>}
           </FormInput>
           <CustomButton disabled={!formValid} styling="blackButton" type="submit">
             SKAPA KONTO
